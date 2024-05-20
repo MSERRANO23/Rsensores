2bf3ec45de45ad0c6a1f00c200d4ef53a74102c8
MSERRANO23 <mserranoperez23@gmail.com>
2024-05-20 19:18:17 +0200

    Update Data_Processing.ino

--- Project/Data_Processing/Data_Processing.ino
+++ Project/Data_Processing/Data_Processing.ino
@@ -10,8 +10,8 @@ int p = 0;  //permaneciendo
 int length = 0;
 
 // Derivadas
-int dAcel = 0;
-int dGiro = 0;
+float dAcel = 0;
+float dGiro = 0;
 
 // Umbrales
 int umbral = 1000;
@@ -64,6 +64,7 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("----------------------");
   // Interrupción
   if (flag == true) {
     IMU.readAcceleration(x_acel, y_acel, z_acel);
@@ -73,92 +74,26 @@ void loop() {
     // Guardo el giro en Y
     yg = z_giro;  // giro en dps
 
-    Serial.println("ok");
+    
   }
   flag = false;
-
+  Serial.println(za);
+  Serial.println(yg);
+  Serial.println("++++++++++");
   // Maquina de estados
   // Cálculo de la derivada
-  dAcel = (za - za_vec[i]) / tiempo_interrupcion;  // derivada subiendo o bajando < 1000
-  dGiro = (yg - yg_vec[i]) / tiempo_interrupcion;  // derivada arriba o en reposo < 1000
-
-  // Estado 1: Reposo (Cuando la derivada de la aceleracion es 0 y aceleracion no supera un umbral por arriba (th1) y por abajo (th2))
-  if (dGiro < umbral) {
-    max_giro = 0;
-    min_giro = 20;
-    acc_giro = 0;
-
-    s = 0;
-    a = 0;
-    if (p != 0) {
-      tiempo_permanencia = p * tiempo_interrupcion;
-    }
-    p = 0;
-    Serial.println("E1");
-  }
-
-  // Estado 2: Subida (Cuando la derivada de la aceleracion es mayor de 0 y aceleracion supera un umbral (th1) y no supera umbral (th3))
-  else if (dAcel < umbral) {
-    subida[s] = za;
-    s++;
-    p++;
-    Serial.println("E2");
-  }
-
-  // Estado 3: Arriba (Cuando la derivada de la aceleracion es 0 y aceleracion supera un umbral por arriba (th4))
-  else if (dGiro < umbral) {
-    if (s != 0) {
-      tiempo_subida = s * tiempo_interrupcion;
-    }
-    s = 0;
-    arriba[a] = yg;
-    a++;
-    p++;
-    Serial.println("E3");
-  }
-
-  // Estado 4: Bajada (Cuando la derivada de la aceleracion es menor de 0 y aceleracion no supera un umbral por arriba y por abajo)
-  else if (dAcel < umbral) {
-    a = 0;
-    p++;
-
-    if (max_giro == 0) {
-
-      length = sizeof(arriba) / sizeof(arriba[0]);
-      for (int j = 0; j < length; i++) {
-        if (arriba[j] < min_giro) {
-          min_giro = arriba[j];
-        }
-        if (arriba[j] > max_giro) {
-          max_giro = arriba[j];
-        }
-        acc_giro += arriba[j];  //acumulado para el giro medio.
-      }
-
-      giro_max = max_giro * (tiempo_interrupcion);
-      giro_min = min_giro * (tiempo_interrupcion);
-      giro_med = acc_giro / length * tiempo_interrupcion;
-      Serial.println("E4");
-    }
-  }
-
-  if (i == 5000) {
-    za_vec[0] = za;
-    yg_vec[0] = yg;
-    i = 0;
-
-  } else {
-    za_vec[i + 1] = za;
-    yg_vec[i + 1] = yg;
-    i++;
-  }
-
-
-  // Cambiar lo siguiente por lo de bluetooth
-
-  Serial.println(tiempo_subida);
-  Serial.println(tiempo_permanencia);
-  Serial.println(giro_max);
-  Serial.println(giro_min);
-  Serial.println(giro_med);
+  dAcel = (za - za_vec[i-1]) / tiempo_interrupcion;  // derivada subiendo o bajando < 1000
+  dGiro = (yg - yg_vec[i-1]) / tiempo_interrupcion;  // derivada arriba o en reposo < 1000
+  Serial.println(dAcel*1000);
+  Serial.println(dGiro*1000);
+  delay(500);
+  yg_vec[i] = yg;
+  za_vec[i] = za;
+  Serial.println(za_vec[i-1]);
+  Serial.println(yg_vec[i-1]);
+  Serial.println(i);
+  i++;
+  // derivada de giro < 6 para considerar que no varia
+  //derivada de aceleración < 0.3 para considerar que no varia
+  
 }
